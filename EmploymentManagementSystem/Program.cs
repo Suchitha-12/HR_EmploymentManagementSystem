30f1d84daa21bcbf24a50d05e07b84a330d712c7
Suchitha-12 <suchithagandham12@gmail.com>
2026-02-20 12:50:13 +0530

    changed the appsetting and url updation with respect to backend and frontend

--- EmploymentManagementSystem/Program.cs
+++ EmploymentManagementSystem/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000", "http://localhost:5002")
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials();
