0a7ac16278952d0992c0dd9aea72fe62765f903a
Suchitha-12 <suchithagandham12@gmail.com>
2026-02-23 17:32:38 +0530

    space changes

--- EmploymentManagementSystem/Services/IReportService.cs
+++ EmploymentManagementSystem/Services/IReportService.cs
@@ -3,8 +3,8 @@
     public interface IReportService
     {
         Task<byte[]> GenerateEmployeeDirectoryPdfAsync();   
-        Task<byte[]> GenerateEmployeeDirectoryExcelAsync();
-        Task<byte[]> GenerateDepartmentReportPdfAsync();
+        Task<byte[]> GenerateEmployeeDirectoryExcelAsync();    
+        Task<byte[]> GenerateDepartmentReportPdfAsync();      
         Task<byte[]> GenerateDepartmentReportExcelAsync();
         Task<byte[]> GenerateAttendanceReportPdfAsync(DateTime? startDate, DateTime? endDate);
         Task<byte[]> GenerateAttendanceReportExcelAsync(DateTime? startDate, DateTime? endDate);
